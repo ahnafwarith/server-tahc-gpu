b92211de04b5684e2494d1fea26798869e99a212
ahnafwarith <corp.ahnafwarid@gmail.com>
2022-05-25 18:43:02 +0600

    loaded parts from db

--- index.js
+++ index.js
@@ -10,8 +10,37 @@ const port = process.env.PORT || 1000;
 app.use(cors());
 app.use(express.json());
 
+// MongoDB
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cfemw.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+// Run
+async function run() {
+    try {
+        await client.connect()
+        // Collections
+        const gpusCollection = client.db('tahc-re-lab').collection('gpus')
+
+        // Verify Admin custom middleware
 
 
+        // ----------------------------- //
+        // CRUD operations
+        // -------------------- //
+
+        // loading parts
+        app.get('/parts', async (req, res) => {
+            const result = await gpusCollection.find().toArray()
+            res.send(result)
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir)
 
 
 // Home
