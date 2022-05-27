69cdc9fdd07beaf78332c3cd93f05f61ad3f7154
ahnafwarith <corp.ahnafwarid@gmail.com>
2022-05-28 05:27:57 +0600

    JWT reviews

--- index.js
+++ index.js
@@ -10,6 +10,27 @@ const port = process.env.PORT || 1000;
 app.use(cors());
 app.use(express.json());
 
+// Verify Json Web Token
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    /* 1st layer of protection (checking for token, if no token found will return the user --> not continue in API call) */
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorized access' })
+    }
+    /* 2nd layer of protection */
+    const token = authHeader.split(' ')[1];
+    // verify a token symmetric
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            // if error, means token doesn't match so access is forbidden
+            return res.status(403).send({ message: 'forbidden access' })
+        }
+        req.decoded = decoded
+        next()
+    });
+}
+
+
 // MongoDB
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -23,24 +44,46 @@ async function run() {
         // Collections
         const gpusCollection = client.db('tahc-re-lab').collection('gpus')
         const bookingCollection = client.db("tahc-re-lab").collection("bookings")
+        const reviewCollection = client.db("tahc-re-lab").collection("reviews")
         // Verify Admin custom middleware
 
 
         // ----------------------------- //
         // CRUD operations
         // -------------------- //
-        // Loading one part based on id
+
+        // Parts: Loading one part based on id
         app.get('/parts/:id', async (req, res) => {
             const id = req.params.id;
             const filter = { _id: ObjectId(id) }
             const result = await gpusCollection.findOne(filter)
             res.send(result)
         })
-        // Loading all parts
+        // Parts: Loading all parts
         app.get('/parts', async (req, res) => {
             const result = await gpusCollection.find().toArray()
             res.send(result)
         })
+
+        // Bookings: posting a booking
+        app.post('/bookings', async (req, res) => {
+            const booking = req.body;
+            const result = await bookingCollection.insertOne(booking);
+            res.send(result);
+        })
+
+        // Reviews : posting a review
+        app.post('/reviews', async (req, res) => {
+            const review = req.body;
+            const result = await reviewCollection.insertOne(review);
+            res.send(result);
+        })
+        // Reviews : Loading all reviews
+        app.get('/reviews', async (req, res) => {
+            const result = await reviewCollection.find().toArray()
+            res.send(result)
+        })
+
     }
     finally {
 
